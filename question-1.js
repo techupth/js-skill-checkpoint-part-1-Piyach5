016c780ff6c301d12cd910cfb805c8b9309e8e84
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-05-22 16:00:14 +0700

    Skill-Checkpoint-JS-Golf

--- question-1.js
+++ question-1.js
@@ -6,3 +6,21 @@ const myTodo = [
   { id: 4, todo: "Wash dishes" },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+const addTodo = (arr, text) => {
+  return arr.push({ id: myTodo.length + 1, todo: text });
+};
+
+addTodo(myTodo, "Walk the dog");
+
+const editTodo = (arr, id, text) =>
+  arr.map((item) => (item.id == id ? (item.todo = text) : item.todo));
+
+editTodo(myTodo, 4, "Go to the gym");
+
+myTodo.pop();
+
+console.log(
+  `To-do: id: ${myTodo[myTodo.length - 1].id}, ${
+    myTodo[myTodo.length - 1].todo
+  }`
+);
--- question-5.js
+++ question-5.js
@@ -17,5 +17,30 @@ const products = [
   },
 ];
 
-const promotionCode = "";
+const promotionCode = "SALE50";
 // เริ่มเขียนโค้ดตรงนี้
+
+const calculateTotalPrice = (products, promotionCode) => {
+  const total = products.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  let discount = 0;
+  function discountFunc(promotionCode) {
+    if (promotionCode == "SALE50") {
+      return (discount = 50);
+    } else if (promotionCode == "SALE20") {
+      return (discount = 20);
+    } else {
+      return (discount = 0);
+    }
+  }
+
+  discountFunc(promotionCode);
+  console.log(discount);
+
+  const totalPrice = total - (total * discount) / 100;
+  return console.log(totalPrice);
+};
+
+calculateTotalPrice(products, promotionCode);
